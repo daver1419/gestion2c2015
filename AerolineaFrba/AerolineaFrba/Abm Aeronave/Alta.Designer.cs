96fabcdb8a7b551256002f20885e37b8ab411f43
alan07sl <alan.07-sl@hotmail.com>
2015-11-11 01:03:29 -0300

    Alta Aeronae en progreso.

---
+++ AerolineaFrba/AerolineaFrba/Abm Aeronave/Alta.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace AerolineaFrba.Abm_Aeronave
+{
+    partial class Alta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DateAlta = new System.Windows.Forms.DateTimePicker();
+            this.FechaAlta = new System.Windows.Forms.Label();
+            this.Modelo = new System.Windows.Forms.Label();
+            this.Fabricante = new System.Windows.Forms.Label();
+            this.Matricula = new System.Windows.Forms.Label();
+            this.TipoServicio = new System.Windows.Forms.Label();
+            this.KG = new System.Windows.Forms.Label();
+            this.Butacas = new System.Windows.Forms.Label();
+            this.ComboFabricante = new System.Windows.Forms.ComboBox();
+            this.TextModelo = new System.Windows.Forms.TextBox();
+            this.TextMatricula = new System.Windows.Forms.TextBox();
+            this.ComboTipoServicio = new System.Windows.Forms.ComboBox();
+            this.NumericKG = new System.Windows.Forms.NumericUpDown();
+            this.Limpiar = new System.Windows.Forms.Button();
+            this.Guardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericKG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DateAlta
+            // 
+            this.DateAlta.Location = new System.Drawing.Point(141, 25);
+            this.DateAlta.Name = "DateAlta";
+            this.DateAlta.Size = new System.Drawing.Size(200, 20);
+            this.DateAlta.TabIndex = 0;
+            // 
+            // FechaAlta
+            // 
+            this.FechaAlta.AutoSize = true;
+            this.FechaAlta.Location = new System.Drawing.Point(23, 25);
+            this.FechaAlta.Name = "FechaAlta";
+            this.FechaAlta.Size = new System.Drawing.Size(73, 13);
+            this.FechaAlta.TabIndex = 1;
+            this.FechaAlta.Text = "Fecha de Alta";
+            // 
+            // Modelo
+            // 
+            this.Modelo.AutoSize = true;
+            this.Modelo.Location = new System.Drawing.Point(23, 94);
+            this.Modelo.Name = "Modelo";
+            this.Modelo.Size = new System.Drawing.Size(42, 13);
+            this.Modelo.TabIndex = 2;
+            this.Modelo.Text = "Modelo";
+            // 
+            // Fabricante
+            // 
+            this.Fabricante.AutoSize = true;
+            this.Fabricante.Location = new System.Drawing.Point(23, 63);
+            this.Fabricante.Name = "Fabricante";
+            this.Fabricante.Size = new System.Drawing.Size(57, 13);
+            this.Fabricante.TabIndex = 3;
+            this.Fabricante.Text = "Fabricante";
+            // 
+            // Matricula
+            // 
+            this.Matricula.AutoSize = true;
+            this.Matricula.Location = new System.Drawing.Point(23, 125);
+            this.Matricula.Name = "Matricula";
+            this.Matricula.Size = new System.Drawing.Size(50, 13);
+            this.Matricula.TabIndex = 4;
+            this.Matricula.Text = "Matricula";
+            // 
+            // TipoServicio
+            // 
+            this.TipoServicio.AutoSize = true;
+            this.TipoServicio.Location = new System.Drawing.Point(23, 153);
+            this.TipoServicio.Name = "TipoServicio";
+            this.TipoServicio.Size = new System.Drawing.Size(82, 13);
+            this.TipoServicio.TabIndex = 5;
+            this.TipoServicio.Text = "Tipo de servicio";
+            // 
+            // KG
+            // 
+            this.KG.AutoSize = true;
+            this.KG.Location = new System.Drawing.Point(23, 182);
+            this.KG.Name = "KG";
+            this.KG.Size = new System.Drawing.Size(67, 13);
+            this.KG.TabIndex = 6;
+            this.KG.Text = "Cantidad KG";
+            // 
+            // Butacas
+            // 
+            this.Butacas.AutoSize = true;
+            this.Butacas.Location = new System.Drawing.Point(23, 211);
+            this.Butacas.Name = "Butacas";
+            this.Butacas.Size = new System.Drawing.Size(46, 13);
+            this.Butacas.TabIndex = 7;
+            this.Butacas.Text = "Butacas";
+            // 
+            // ComboFabricante
+            // 
+            this.ComboFabricante.FormattingEnabled = true;
+            this.ComboFabricante.Location = new System.Drawing.Point(141, 60);
+            this.ComboFabricante.Name = "ComboFabricante";
+            this.ComboFabricante.Size = new System.Drawing.Size(200, 21);
+            this.ComboFabricante.TabIndex = 8;
+            // 
+            // TextModelo
+            // 
+            this.TextModelo.Location = new System.Drawing.Point(141, 87);
+            this.TextModelo.Name = "TextModelo";
+            this.TextModelo.Size = new System.Drawing.Size(200, 20);
+            this.TextModelo.TabIndex = 9;
+            // 
+            // TextMatricula
+            // 
+            this.TextMatricula.Location = new System.Drawing.Point(141, 118);
+            this.TextMatricula.Name = "TextMatricula";
+            this.TextMatricula.Size = new System.Drawing.Size(200, 20);
+            this.TextMatricula.TabIndex = 10;
+            // 
+            // ComboTipoServicio
+            // 
+            this.ComboTipoServicio.FormattingEnabled = true;
+            this.ComboTipoServicio.Location = new System.Drawing.Point(141, 150);
+            this.ComboTipoServicio.Name = "ComboTipoServicio";
+            this.ComboTipoServicio.Size = new System.Drawing.Size(200, 21);
+            this.ComboTipoServicio.TabIndex = 11;
+            // 
+            // NumericKG
+            // 
+            this.NumericKG.Location = new System.Drawing.Point(141, 182);
+            this.NumericKG.Name = "NumericKG";
+            this.NumericKG.Size = new System.Drawing.Size(200, 20);
+            this.NumericKG.TabIndex = 12;
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.Location = new System.Drawing.Point(141, 292);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(75, 23);
+            this.Limpiar.TabIndex = 13;
+            this.Limpiar.Text = "Limpiar";
+            this.Limpiar.UseVisualStyleBackColor = true;
+            this.Limpiar.Click += new System.EventHandler(this.Limpiar_Click);
+            // 
+            // Guardar
+            // 
+            this.Guardar.Location = new System.Drawing.Point(266, 292);
+            this.Guardar.Name = "Guardar";
+            this.Guardar.Size = new System.Drawing.Size(75, 23);
+            this.Guardar.TabIndex = 14;
+            this.Guardar.Text = "Guardar";
+            this.Guardar.UseVisualStyleBackColor = true;
+            this.Guardar.Click += new System.EventHandler(this.Guardar_Click);
+            // 
+            // Alta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 363);
+            this.Controls.Add(this.Guardar);
+            this.Controls.Add(this.Limpiar);
+            this.Controls.Add(this.NumericKG);
+            this.Controls.Add(this.ComboTipoServicio);
+            this.Controls.Add(this.TextMatricula);
+            this.Controls.Add(this.TextModelo);
+            this.Controls.Add(this.ComboFabricante);
+            this.Controls.Add(this.Butacas);
+            this.Controls.Add(this.KG);
+            this.Controls.Add(this.TipoServicio);
+            this.Controls.Add(this.Matricula);
+            this.Controls.Add(this.Fabricante);
+            this.Controls.Add(this.Modelo);
+            this.Controls.Add(this.FechaAlta);
+            this.Controls.Add(this.DateAlta);
+            this.Name = "Alta";
+            this.Text = "Alta de aeronave";
+            this.Load += new System.EventHandler(this.Alta_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NumericKG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker DateAlta;
+        private System.Windows.Forms.Label FechaAlta;
+        private System.Windows.Forms.Label Modelo;
+        private System.Windows.Forms.Label Fabricante;
+        private System.Windows.Forms.Label Matricula;
+        private System.Windows.Forms.Label TipoServicio;
+        private System.Windows.Forms.Label KG;
+        private System.Windows.Forms.Label Butacas;
+        private System.Windows.Forms.ComboBox ComboFabricante;
+        private System.Windows.Forms.TextBox TextModelo;
+        private System.Windows.Forms.TextBox TextMatricula;
+        private System.Windows.Forms.ComboBox ComboTipoServicio;
+        private System.Windows.Forms.NumericUpDown NumericKG;
+        private System.Windows.Forms.Button Limpiar;
+        private System.Windows.Forms.Button Guardar;
+    }
+}
---
+++ AerolineaFrba/AerolineaFrba/Abm Aeronave/Alta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AerolineaFrba.DAO;
+using AerolineaFrba.DTO;
+
+namespace AerolineaFrba.Abm_Aeronave
+{
+    public partial class Alta : Form
+    {
+        private AeronaveDTO Aeronave;
+
+        public Alta()
+        {
+            InitializeComponent();
+        }
+
+        private void Alta_Load(object sender, EventArgs e)
+        {
+            Aeronave = new AeronaveDTO();
+            ComboFabricante.DataSource = FabricanteDAO.selectAll();
+            ComboFabricante.SelectedIndex = -1;
+            ComboTipoServicio.DataSource = TipoServicioDAO.selectAll();
+            ComboTipoServicio.SelectedIndex = -1;
+        }
+
+        private void Limpiar_Click(object sender, EventArgs e)
+        {
+            TextMatricula.Text = "";
+            TextModelo.Text = "";
+            NumericKG.Value = 0;
+            DateAlta.Value = DateTime.Now;
+            ComboFabricante.SelectedIndex = -1;
+            ComboTipoServicio.SelectedIndex = -1;
+        }
+
+        private void Guardar_Click(object sender, EventArgs e)
+        {
+            Aeronave.Fabricante = ((FabricanteDTO)ComboFabricante.SelectedValue);
+            Aeronave.TipoServicio = ((TipoServicioDTO)ComboTipoServicio.SelectedValue);
+            Aeronave.FechaAlta = DateAlta.Value;
+            Aeronave.KG = Decimal.ToInt32(NumericKG.Value);
+            Aeronave.Matricula = TextMatricula.Text;
+            Aeronave.Modelo = TextModelo.Text;
+
+            AeronaveDAO.AltaAeronave(Aeronave);
+        }
+
+    }
+}
